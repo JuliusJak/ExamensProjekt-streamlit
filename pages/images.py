7000b33197d69dbed9950eb8aa69ce85cb50e7d7
JuliusJak <julius.bis03@gmail.com>
2024-05-21 15:01:14 +0200

    added simple image clicking practice

--- pages/images.py
+++ pages/images.py
@@ -2,8 +2,24 @@ import streamlit as st
 from components.navBar import navBar
 from streamlit_image_coordinates import streamlit_image_coordinates
 
+
+st.set_page_config(
+    page_title="Image practice",
+    page_icon=None,
+    layout="wide"
+)
 navBar()
+st.title("Image practice")
+with st.container(border=True):
+    st.subheader("Click on the battery")
+    value = streamlit_image_coordinates("images/car_engine.jpg")
 
-value = streamlit_image_coordinates("images\cute_cat1.jpg")
-
-st.write(value)
+    if value:
+        x, y = value["x"], value["y"]
+        if 710 <= x <= 910 and 315 <= y <= 455:
+            st.success("Correct! The coordinates are within the specified range.")
+        else:
+            st.error("Incorrect. The coordinates are outside the specified range.")
+        st.write(f"Clicked coordinates: x={x}, y={y}")
+    else:
+        st.write("Click on the image to get coordinates.")
