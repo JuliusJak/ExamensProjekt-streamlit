aedd4185c7a11e2b5f30eb0a0145a35d023cd825
JuliusJak <julius.bis03@gmail.com>
2024-05-27 13:17:35 +0200

    Users can now practice with any image question

--- api_requests/images_api.py
+++ api_requests/images_api.py
@@ -17,3 +17,26 @@ def save_image_question(image_question):
 
     return response
 
+
+def get_image_questions(id=None, name=None, question=None):
+    
+    url = "http://localhost:8082/image-questions/search"
+    params = {
+        'id': id,
+        'name': name,
+        'question': question
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        print("ImageQuestions retrieved successfully.")
+        print(response.json())
+    elif response.status_code == 404:
+        print("No ImageQuestions found.")
+    else:
+        print(f"Failed to retrieve ImageQuestions. Status code: {response.status_code}")
+        print(f"Response: {response.json()}")
+
+    return response.json()
+
--- pages/images_practice.py
+++ pages/images_practice.py
@@ -2,7 +2,7 @@ import streamlit as st
 from components.navBar import navBar
 from streamlit_image_coordinates import streamlit_image_coordinates
 from authentication import authentication
-
+from api_requests.images_api import get_image_questions
 
 st.set_page_config(
     page_title="Image practice",
@@ -13,17 +13,42 @@ authentication()
 navBar()
 st.title("Image practice")
 with st.container(border=True):
-    st.subheader("Click on the battery")
+
+
+    all_image_questions = get_image_questions()
+
+    questions = []
+    for question in all_image_questions:
+        questions.append(question["question"])
+
+    if "selectbox_index" not in st.session_state:
+        st.session_state["selectbox_index"] = 0
+
+    selected_image_question = st.selectbox("Select A Image Question",questions,index=st.session_state["selectbox_index"])
+
+    image_question = get_image_questions(question=selected_image_question)
+    
+    st.subheader(image_question[0]["question"])
+
     width = 700
     height = width * 0.667
-    value = streamlit_image_coordinates("images/car_engine.jpg",height,width)
+    value = streamlit_image_coordinates(f"images/{image_question[0]['name']}.jpg",height,width)
 
     if value:
         x, y = value["x"], value["y"]
-        if 710 <= x <= 910 and 315 <= y <= 455:
-            st.success("Correct! The coordinates are within the specified range.")
+        
+        min_x = min(image_question[0]["firstXValue"], image_question[0]["secondXValue"])
+        max_x = max(image_question[0]["firstXValue"], image_question[0]["secondXValue"])
+        min_y = min(image_question[0]["firstYValue"], image_question[0]["secondYValue"])
+        max_y = max(image_question[0]["firstYValue"], image_question[0]["secondYValue"])
+
+        if min_x <= x <= max_x and min_y <= y <= max_y:
+            st.success("Correct")
         else:
-            st.error("Incorrect. The coordinates are outside the specified range.")
-        st.write(f"Clicked coordinates: x={x}, y={y}")
-    else:
-        st.write("Click on the image to get coordinates.")
+            st.error("Wrong")
+
+    if st.button("Next Question"):
+        st.session_state["selectbox_index"] += 1
+        if st.session_state["selectbox_index"] >= len(questions):
+            st.session_state["selectbox_index"] = 0
+        st.rerun()
